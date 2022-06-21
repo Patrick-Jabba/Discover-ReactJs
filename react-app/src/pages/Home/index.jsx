3f89103a79a8c3ac5a82917bb2ea0034c2a54ba9
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-06-21 15:17:00 -0300

    fundamentos de react

---
+++ react-app/src/pages/Home/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+
+import { Card } from '../../components/Card';
+
+export function Home() {
+  const [studentName, setStudentName] = useState('Valor inicial do Estado');
+  const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name: '', avatar: ''});
+
+  function handleAddStudent() {
+    const newStudent = {
+      id: Math.random(),
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/patrick-jabba')
+    .then(response => response.json())
+    .then(data => {
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })       
+    });
+  }, []);
+  //  |  dependência do useEffect
+
+  return (
+    <div className='container'>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar}alt="profile pic" />
+        </div>
+      </header>
+      <input
+        placeholder="Digite o nome: "
+        type="text"
+        onChange={e => setStudentName(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={handleAddStudent}
+      >
+        Adicionar
+      </button>
+      {
+        students.map(student =>
+          <Card
+            key={student.id}
+            name={student.name}
+            time={student.time}
+          />
+        )
+      }
+    </div>
+  )
+}
+
+
